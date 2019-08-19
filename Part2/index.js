cb0ef5996ff3fa24247d34fcf51423b4639e8a35
taylor dearden <taylordearden@gmail.com>
2019-08-19 15:49:34 -0600

    diamond part 2

--- Part2/index.js
+++ Part2/index.js
@@ -10,3 +10,20 @@ function setCard() {
     const card = document.getElementById(idInput.value)
     card.style.color = colorInput.value
 }
+
+
+function resetCard () {
+   const hearts = document.getElementById('hearts') 
+   hearts.style.color = 'grey'
+   const spades = document.getElementById('spades') 
+   spades.style.color = 'grey'
+   const diamonds = document.getElementById('diamonds') 
+   diamonds.style.color = 'grey'
+   const clubs = document.getElementById('clubs') 
+   clubs.style.color = 'grey'
+}
+
+
+function resetCard2 ()
+{location.reload(true)
+}
