6e4a862bd2f93651f257bddcd05ecaa19d589aea
taylor dearden <taylordearden@gmail.com>
2019-08-19 16:26:37 -0600

    finished everything plus black diamond

--- Part1/index.js
+++ Part1/index.js
@@ -26,37 +26,37 @@ function play(clickedId) {
     const bottomCenter = board[7];
     const bottomRight = board[8];
 
-    if (topLeft !== undefined && topCenter & topLeft === topRight) {
+    if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
         alert(`${topLeft} is the winner!`);
-        return;
+        return reset();
     }
     if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
         alert(`${middleLeft} is the winner!`);
-        return;
+        return reset();
     }
     if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
         alert(`${bottomLeft} is the winner!`);
-        return;
+        return reset();
     }
     if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
         alert(`${topLeft} is the winner!`);
-        return;
+         return reset();
     }
     if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
         alert(`${topCenter} is the winner!`);
-        return;
+         return reset();
     }
     if (topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
         alert(`${topRight} is the winner!`);
-        return;
+         return reset();
     }
     if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
         alert(`${topLeft} is the winner!`);
-        return;
+         return reset() ;
     }
     if (bottomLeft !== undefined && bottomLeft === middleCenter && bottomLeft === topRight) {
         alert(`${bottomLeft} is the winner!`);
-        return;
+        return reset();
     }
     let boardFull = true;
     for (let i = 0; i <= 8; i++) {
@@ -66,9 +66,13 @@ function play(clickedId) {
     }
     if (boardFull === true) {
         alert("Cat's game, there is no winner :(")
+        return reset()
     }
 }
 
+function reset(){
+ location.reload(true)
+}
 
 
 
